8c5c5f4c7ddd7f89d01028e3b28a2fc5002e1c14
Sam Atman <atmanistan@gmail.com>
2025-12-28 10:56:47 -0500

    Basic rule definitions

--- grammar.js
+++ grammar.js
@@ -12,6 +12,46 @@ export default grammar({
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => repeat($._declaration),
+
+    _declaration: ($) =>
+      choice(
+        $.grammar_rule,
+        // $.directive
+      ),
+
+    grammar_rule: ($) =>
+      seq($._nonterminal_m_alias, "::=", repeat($._production), "."),
+
+    _nonterminal_m_alias: ($) =>
+      seq(field("rule_name", $.nonterminal), field("alias", optional($.alias))),
+
+    _production: ($) =>
+      choice(
+        $._nonterminal_m_alias,
+        $._terminal_m_alias,
+        $._multiterminal_m_alias,
+      ),
+
+    _terminal_m_alias: ($) =>
+      prec(
+        2,
+        seq(field("rule_name", $.terminal), field("alias", optional($.alias))),
+      ),
+
+    _multiterminal_m_alias: ($) =>
+      seq(
+        field("rule_name", $.multiterminal),
+        field("alias", optional($.alias)),
+      ),
+
+    nonterminal: ($) => /[a-z][a-zA-Z0-9_]*/,
+
+    terminal: ($) => /[A-Z][a-zA-Z0-9_]*/,
+
+    multiterminal: ($) =>
+      seq($.terminal, repeat(seq(choice("|", "/"), $.terminal))),
+
+    alias: ($) => seq("(", /[A-Za-z][A-Za-z0-9_]*/, ")"),
+  },
 });
